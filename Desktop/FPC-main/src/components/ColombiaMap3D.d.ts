52631b6e22f3dd644e50f9218e79cf430245a7d3
edben110 <160884920+edben110@users.noreply.github.com>
2025-11-02 17:25:48 -0500

    Delete Desktop/FPC-main directory

--- Desktop/FPC-main/src/components/ColombiaMap3D.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-
-declare const ColombiaMap3D: React.FC;
-export default ColombiaMap3D;
--- Desktop/FPC-main/src/components/InteractiveGeo3D.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-
-declare const InteractiveGeo3D: React.FC;
-export default InteractiveGeo3D;
