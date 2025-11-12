c4790a08fe7db0a845f7509b20c5b92a105ace7a
JUanM776 <Juan.cordobaflore@campusucc.edu.co>
2025-11-12 09:40:53 -0500

    Se corrigio ColombiaMap

--- src/components/__tests__/ColombiaMap3D.test.tsx
+++ src/components/__tests__/ColombiaMap3D.test.tsx
@@ -60,7 +60,7 @@ describe("ColombiaMap3D Component", () => {
   test("puede renderizarse múltiples veces sin error", () => {
     const { rerender } = render(<ColombiaMap3D />);
     rerender(<ColombiaMap3D />);
-    expectscreen.getByTestId("colombiamap3d-placeholder")).toBeTruthy();
+    expect(screen.getByTestId("colombiamap3d-placeholder")).toBeTruthy();
   });
 
   test("el placeholder tiene el contenido correcto", () => {
